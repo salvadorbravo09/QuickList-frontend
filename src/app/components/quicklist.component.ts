b178217bd185cd028b0a74498fc8f893cddd1a47
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-09 18:02:41 -0400

    tabla basica html con la informacion de usuario

--- src/app/components/quicklist.component.ts
+++ src/app/components/quicklist.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-quicklist',
   standalone: true,
   imports: [],
-  templateUrl: './quicklist.component.html'
+  templateUrl: './quicklist.component.html',
 })
-export class QuicklistComponent {
-
+export class QuicklistComponent implements OnInit {
   title: string = 'Listado de Usuarios';
+  users: User[] = [];
+
+  constructor(private service: UserService) {}
+  
+  ngOnInit(): void {
+    this.service.findAll().subscribe((users) => {
+      this.users = users;
+    });
+  }
 }
