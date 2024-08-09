b042180fb2dd8f6162f6290a15b933ad5383b037
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-09 18:07:18 -0400

    importamos el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuicklistComponent } from './components/quicklist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuicklistComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'quicklist';
