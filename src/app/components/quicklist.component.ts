50fd4c38ed06a9e32a7c98d3959649583b61b5bb
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-09 17:42:52 -0400

    se creo el componente principal

---
+++ src/app/components/quicklist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quicklist',
+  standalone: true,
+  imports: [],
+  templateUrl: './quicklist.component.html'
+})
+export class QuicklistComponent {
+
+  title: string = 'Listado de Usuarios';
+}
