193ebe834dcc4088a089c1664b47f7e9c7375362
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-14 21:07:44 -0400

    se agrego la comunicacion con el backend en el metodo get find

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,33 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private users: User[] = [
-    {
-      id: 1,
-      name: 'Salvador',
-      lastname: 'Bravo',
-      email: 'sa.bravo0901@gmail.com',
-      username: 'sa.bravo',
-      password: '12345',
-    },
-    {
-      id: 2,
-      name: 'Yara',
-      lastname: 'Carrillo',
-      email: 'yara@gmail.com',
-      username: 'yarita',
-      password: '12345',
-    },
-  ];
+  private users: User[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   findAll(): Observable<User[]> {
-    return of(this.users);
+    return this.http.get<User[]>('http://localhost:8080/api/users/find');
   }
 }
